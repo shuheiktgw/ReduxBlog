4923149dd0d7b5cc33797ee5cf9bf5f8ffcd0560
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-15 17:56:13 +0900

    Add index

---
+++ src/components/post_index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Shuhei on 2016/10/15.
+ */
+
+import React from 'react';
+
+export default () => {
+  return <div>List of blog post</div>
+}
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Shuhei on 2016/10/14.
+ */
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+import App from './components/app';
+import PostIndex from './components/post_index';
+
+export default(
+  <Route path="/" component={App} >
+    <IndexRoute component={PostIndex}/>
+  </Route>
+);
+
